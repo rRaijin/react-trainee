611f440fbb695258a8616343bee8fc9be1392267
Fedoryaka Vladimir <you.bestof.thebest@gmail.com>
2018-10-28 16:32:27 +0200

    Static (#5)

--- newReact/settings.py
+++ newReact/settings.py
@@ -15,10 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'foo-secret'
 
@@ -26,9 +22,6 @@ SECRET_KEY = 'foo-secret'
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -142,9 +135,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
--- newReact/settings_prod.py
+++ newReact/settings_prod.py
@@ -1,4 +1,6 @@
 import os
+from django.conf import settings
+
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = [u'notfacebook.pythonanywhere.com']
@@ -6,10 +8,16 @@ ALLOWED_HOSTS = [u'notfacebook.pythonanywhere.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("DB_NAME"),
+        'NAME': "notfacebook$notfacebook",
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': 3306
     }
 }
+
+
+MEDIA_ROOT = os.path.join(settings.BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(settings.BASE_DIR, 'static')
+STATIC_URL = '/static/'
