3a0d8fc90aab0de5394677f04b43d0e83de6512b
Vladimir <fedoryakavladimir@gmail.com>
2018-10-28 14:00:44 +0200

    dbs settings

--- newReact/settings_prod.py
+++ newReact/settings_prod.py
@@ -2,3 +2,14 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = [u'notfacebook.pythonanywhere.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': 3306
+    }
+}
