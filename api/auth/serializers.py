c560ce9f614e9527b688b55f6e804e7478078e8c
roman <realakira@ukr.net>
2018-10-30 08:34:37 +0200

    feature-auth-validation-update

--- api/auth/serializers.py
+++ api/auth/serializers.py
@@ -25,4 +25,4 @@ class LoginUserSerializer(serializers.Serializer):
         user = authenticate(**data)
         if user and user.is_active:
             return user
-        raise serializers.ValidationError("Unable to log in with provided credentials.")
+        raise serializers.ValidationError("username or password is incorrect.")
--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -14,7 +14,6 @@ class Home extends Component {
     };
 
     render() {
-        console.log(this.props);
         return (
             <div className="row">
                 <div className="col-lg-8">
